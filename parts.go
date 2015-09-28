da0e4abc793b95a9baed48f0273bea4cbd35b845
kusabashira <kusabashira227@gmail.com>
2015-09-28 20:43:12 +0900

    Unescape escaped comma

--- parts.go
+++ parts.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"regexp"
+	"strings"
 )
 
 var KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
 
 func ParseKeysList(list string) []string {
-	return KEYS_LIST.FindAllString(list, -1)
+	keys := KEYS_LIST.FindAllString(list, -1)
+	for i := 0; i < len(keys); i++ {
+		keys[i] = strings.Replace(keys[i], `\,`, `,`, -1)
+	}
+	return keys
 }
