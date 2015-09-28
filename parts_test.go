ba8cd9b8e547d1e0de32a05ad79a7f4af92e3907
kusabashira <kusabashira227@gmail.com>
2015-09-28 19:52:05 +0900

    Add test for ParseKeysList

--- parts_test.go
+++ parts_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseKeysList(t *testing.T) {
+	list := "host,status"
+	expect := []string{"host", "status"}
+	actual := ParseKeysList(list)
+	if !reflect.DeepEqual(actual, expect) {
+		t.Errorf("ParseKeysList(%q) = %q, want %q",
+			list, actual, expect)
+	}
+}
