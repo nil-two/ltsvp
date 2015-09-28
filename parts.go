b32c24bb71342a58c7eda149033756895bc012fa
kusabashira <kusabashira227@gmail.com>
2015-09-28 19:53:47 +0900

    Implement ParseKeysList as rough

--- parts.go
+++ parts.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"regexp"
+)
+
+var KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+
+func ParseKeysList(list string) []string {
+	return KEYS_LIST.FindAllString(list, -1)
+}
