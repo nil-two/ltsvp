04fe58b030ce0de286798a117c11aee2ce76a071
kusabashira <kusabashira227@gmail.com>
2015-09-30 05:21:05 +0900

    Merge main.go and option.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/yuya-takeyama/argf"
@@ -32,6 +34,38 @@ v0.1.0
 
 func printErr(err error) {
 	fmt.Fprintln(os.Stderr, "ltsvp:", err)
+}
+
+type Option struct {
+	List       string
+	Delimiter  string
+	RemainLTSV bool
+	IsHelp     bool
+	IsVersion  bool
+	Files      []string
+}
+
+func ParseOption(args []string) (opt *Option, err error) {
+	f := flag.NewFlagSet("ltsvp", flag.ContinueOnError)
+	f.SetOutput(ioutil.Discard)
+
+	opt = &Option{}
+	f.StringVar(&opt.List, "k", "", "")
+	f.StringVar(&opt.List, "keys", "", "")
+	f.StringVar(&opt.Delimiter, "d", "\t", "")
+	f.StringVar(&opt.Delimiter, "delimiter", "\t", "")
+	f.BoolVar(&opt.RemainLTSV, "r", false, "")
+	f.BoolVar(&opt.RemainLTSV, "remain-ltsv", false, "")
+	f.BoolVar(&opt.IsHelp, "h", false, "")
+	f.BoolVar(&opt.IsHelp, "help", false, "")
+	f.BoolVar(&opt.IsVersion, "v", false, "")
+	f.BoolVar(&opt.IsVersion, "version", false, "")
+
+	if err := f.Parse(args); err != nil {
+		return nil, err
+	}
+	opt.Files = f.Args()
+	return opt, nil
 }
 
 func newLTSVScannerFromOption(opt *Option) (l *LTSVScanner, err error) {
