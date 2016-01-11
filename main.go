eb3e82a9bcaadcf5bd946c4514733e268d10707d
kusabashira <kusabashira227@gmail.com>
2016-01-11 19:33:10 +0900

    Upgrade to fix way to check whether keys specified

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	name    = "ltsvp"
-	version = "0.4.0"
+	version = "0.4.1"
 
 	flag            = pflag.NewFlagSet(name, pflag.ContinueOnError)
 	list            = flag.StringP("keys", "k", "", "")
