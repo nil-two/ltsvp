fd5a393ffbfecb6027931a57da8580cd403565d6
kusabashira <kusabashira227@gmail.com>
2016-02-15 01:19:30 +0900

    Move ParseKeysList to keys.go

--- scanner.go
+++ scanner.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"io"
-	"regexp"
 	"strings"
 
 	"github.com/ymotongpoo/goltsv"
 )
-
-var KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
-
-func ParseKeysList(list string) []string {
-	keys := KEYS_LIST.FindAllString(list, -1)
-	for i := 0; i < len(keys); i++ {
-		keys[i] = strings.Replace(keys[i], `\,`, `,`, -1)
-		keys[i] = strings.Replace(keys[i], `\\`, `\`, -1)
-	}
-	return keys
-}
 
 type LTSVScanner struct {
 	OutputDelimiter string
