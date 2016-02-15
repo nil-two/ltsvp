810afc5c5a19512565f7a7fb52f664cd6b30489f
kusabashira <kusabashira227@gmail.com>
2016-02-15 16:51:38 +0900

    Ignore trailing backslash in LIST

--- keys.go
+++ keys.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"regexp"
+	"strings"
 )
 
 var (
-	KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
-	BACKSLASH = regexp.MustCompile(`\\(.)`)
+	KEYS_LIST          = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+	BACKSLASH          = regexp.MustCompile(`\\(.)`)
+	TRAILING_BACKSLASH = regexp.MustCompile(`\\+$`)
 )
 
 func ParseKeysList(list string) []string {
+	list = TRAILING_BACKSLASH.ReplaceAllStringFunc(list, func(s string) string {
+		return strings.Repeat(`\\`, len(s)/2)
+	})
 	keys := KEYS_LIST.FindAllString(list, -1)
 	for i := 0; i < len(keys); i++ {
 		keys[i] = BACKSLASH.ReplaceAllString(keys[i], "$1")
