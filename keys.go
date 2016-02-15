da0cdfd67aace4e0f747fede56eccb0a03b243d3
kusabashira <kusabashira227@gmail.com>
2016-02-15 14:45:14 +0900

    Interpret "\n" as "n" in LIST

--- keys.go
+++ keys.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"regexp"
-	"strings"
 )
 
-var KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+var (
+	KEYS_LIST = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+	BACKSLASH = regexp.MustCompile(`\\(.)`)
+)
 
 func ParseKeysList(list string) []string {
 	keys := KEYS_LIST.FindAllString(list, -1)
 	for i := 0; i < len(keys); i++ {
-		keys[i] = strings.Replace(keys[i], `\,`, `,`, -1)
-		keys[i] = strings.Replace(keys[i], `\\`, `\`, -1)
+		keys[i] = BACKSLASH.ReplaceAllString(keys[i], "$1")
 	}
 	return keys
 }
